1882096ffa6b4b59b5daf091f2e26f29f1747ac8
suraj-iimv <surajkr.iimv@gmail.com>
2026-04-29 16:56:01 +0530

    Delete logger.py

--- logger.py
+++
@@ -1,6 +0,0 @@
-import datetime
-
-def log_result(text, output):
-    with open("logs.txt", "a") as file:
-        timestamp = datetime.datetime.now()
-        file.write(f"{timestamp} | Input: {text} | Output: {output}\n")
